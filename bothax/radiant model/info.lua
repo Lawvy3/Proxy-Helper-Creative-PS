9a691d6b2d77eab0fc303741dc2c66454fe7f56e
Lawvy3 <154064235+Lawvy3@users.noreply.github.com>
2026-06-09 17:09:59 +0700

    Update info.lua

--- bothax/radiant model/info.lua
+++ bothax/radiant model/info.lua
@@ -1,1 +1,12 @@
-
+return {
+  model = "Radiant",
+  version = "1.0.0",
+  author = "Lawvy",
+  build = "dev",
+  created = "",
+  updated = "",
+  discord = "https://discord.gg/kKPVKbuMvq",
+  changelog = {
+    "`0[`2+`0] `0Proxy released!"
+  }
+}
